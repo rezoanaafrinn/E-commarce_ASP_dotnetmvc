2e8d776c7f743c6e74a7d465bb3fcadd6439f1c5
rezoanaafrinn <rezoanaafrinn@gmail.com>
2023-03-29 15:34:25 +0600

    added new files

---
+++ Etickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace Etickets.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ Etickets/Models/Cinema.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Etickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public string logo { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
